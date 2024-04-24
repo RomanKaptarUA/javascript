02c0a54d2604d74fdc30f0359ca8383a8af29a04
RomanKaptarUA <romankaptar82@gmail.com>
2024-04-24 20:03:39 +0300

    явапрол

---
+++ script.js
@@ -0,0 +1,24 @@
+
+
+const b = 10;
+a = b * 2;
+console.log(a);
+
+console.log("Hello everybody".toLocaleUpperCase());
+
+// let age = 18;
+// age = 19;
+// console.log(age);
+
+// let userName = "Анна";
+// console.log(userName);
+
+// let userName = false;
+// console.log(typeof userName);
+
+// const message = "Привіт";
+// console.log(message);
+// alert(message);
+
+const message = prompt("Чи погоджуєтесь ви з умовами?");
+console.log(message);
