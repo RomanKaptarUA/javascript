76b66d1c49a1c773128aaef241b35c76100b9bae
RomanKaptarUA <romankaptar82@gmail.com>
2024-04-26 18:46:16 +0300

    ролдж

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 
 
-const b = 10;
-a = b * 2;
-console.log(a);
+// const b = 10;
+// a = b * 2;
+// console.log(a);
 
-console.log("Hello everybody".toLocaleUpperCase());
+// console.log("Hello everybody".toLocaleUpperCase());
 
 // let age = 18;
 // age = 19;
@@ -20,5 +20,37 @@ console.log("Hello everybody".toLocaleUpperCase());
 // console.log(message);
 // alert(message);
 
-const message = prompt("Чи погоджуєтесь ви з умовами?");
-console.log(message);
+// const message = prompt("Чи погоджуєтесь ви з умовами?");
+// console.log(message);
+
+const x = 5;
+const y = 2;
+const z = 5;
+
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+console.log(x % y);
+
+// let b = 20;
+// b += 10;
+// console.log(a);
+
+console.log(x > y);
+console.log(x < y);
+console.log(x <- z);
+console.log(x == y);
+console.log(x != z);
+console.log(x === y);
+
+
+
+
+const a="5";
+console.log(Number(a));
+console.log(typeof Number(a));
+const b ="hello";
+console.log(Number(b));
+
+
