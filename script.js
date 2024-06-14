8ae71bb18c6afe45f76f40aa0c015246c1ef5cf5
RomanKaptarUA <romankaptar82@gmail.com>
2024-06-14 19:00:32 +0300

    н6гцрчі

--- script.js
+++ script.js
@@ -648,9 +648,59 @@
 // const arr = fruits.concat(numbers);
 // console.log(arr);
 
-const message = 'Welcome to Ukraine!';
-const arrMess = message.split('');
-console.log(arrMess);
+// const message = 'Welcome to Ukraine!';
+// const arrMess = message.split('');
+// console.log(arrMess);
 
 
 
+
+// функції
+//  const add = function(a, b, c){
+//     return a + b + c;
+// }
+// const result = add(1, 3, 5);
+// console.log(result);
+
+// console.log(add(5, 10, 15));
+
+// const count = function(fromCount = 0, toCount = 5, step = 1) {
+//     console.log(`fromCount = ${fromCount}, toCount ${toCount}, step = ${step}`);
+//     for (let i = fromCount; i <= toCount; i += step){
+//         console.log(i);
+//     }
+// };
+// count(1, 30);
+// count(20, 25);
+
+// const sum = function (amount, balance) {
+//     if (amount === 0) {
+//         console.log('Введіть суму більше нуля');
+//     }
+//     else if (amount > balance) {
+//         console.log('Недостатньо коштів');
+//     } else {
+//         console.log('Операцію проведено');
+//     }
+// };
+
+// sum(10000, 800);
+
+const sum = function (amount, balance) {
+    if ( amount === 0) {
+        console.log('Введіть суму більше нуля');
+        return;
+    }
+    if (amount > balance) {
+        console.log('Недостатньо коштів');
+        return;
+    }
+    console.log('Операцію проведено');
+}
+
+const fn = function () {
+    const arg = Array.from(arguments);
+    conaole.log(arg);
+}
+fn(5, 9, 10);
+fn('s', 'f', 'a');
